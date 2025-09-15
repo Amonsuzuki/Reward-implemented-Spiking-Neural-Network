ccf528678f0c948faaa9bb9ce42b23c72dc12f2b
Amonsuzuki <amonsuzuki@keio.jp>
2025-09-15 16:45:54 +0900

    test0

--- test/test.py
+++ test/test.py
@@ -35,5 +35,6 @@ async def multilayer_test(dut):
 	await ClockCycles(dut.clk, 4)
 
 	assert dut.uo_out.value == 0b00110010
+	assert dut.uo_out.value == 0b00000000
 	dut._log.info("multilayer test passed")
 
